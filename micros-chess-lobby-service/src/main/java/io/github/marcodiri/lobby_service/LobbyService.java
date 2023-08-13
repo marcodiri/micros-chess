d7de4804b389bcf781e8cc4a44fce19cefc64631
marcodiri <marcodiri96@gmail.com>
2023-08-13 09:23:35 +0200

    LobbyService logger

--- micros-chess-lobby-service/src/main/java/io/github/marcodiri/lobby_service/LobbyService.java
+++ micros-chess-lobby-service/src/main/java/io/github/marcodiri/lobby_service/LobbyService.java
@@ -2,6 +2,9 @@ package io.github.marcodiri.lobby_service;
 
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.github.marcodiri.lobby_service.domain.GameProposal;
 import io.github.marcodiri.lobby_service.domain.command.AcceptGameProposalCommand;
 import io.github.marcodiri.lobby_service.domain.command.CancelGameProposalCommand;
@@ -12,24 +15,29 @@ public class LobbyService {
 
     private GameProposalESRepository gameProposalESRepository;
 
+	private static final Logger LOGGER = LogManager.getLogger(LobbyService.class);
+
     public LobbyService(GameProposalESRepository gameProposalESRepository) {
         this.gameProposalESRepository = gameProposalESRepository;
     }
 
     public GameProposal createGameProposal(UUID creatorId) {
         GameProposal gameProposal = gameProposalESRepository.save(new CreateGameProposalCommand(creatorId));
+        LOGGER.info("Created: {}", gameProposal);
         return gameProposal;
     }
 
     public GameProposal cancelGameProposal(UUID gameProposalId, UUID creatorId) {
         GameProposal gameProposal = gameProposalESRepository
                 .update(gameProposalId, new CancelGameProposalCommand(creatorId));
+        LOGGER.info("Canceled: {}", gameProposal);
         return gameProposal;
     }
 
     public GameProposal acceptGameProposal(UUID gameProposalId, UUID acceptorId) {
         GameProposal gameProposal = gameProposalESRepository
                 .update(gameProposalId, new AcceptGameProposalCommand(acceptorId));
+        LOGGER.info("Accepted: {}", gameProposal);
         return gameProposal;
     }
 
