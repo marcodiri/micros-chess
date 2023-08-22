121412e8b29d28cfbf87d0f5511bf26f2c5b2081
marcodiri <marcodiri96@gmail.com>
2023-08-22 06:47:13 +0000

    Integration tests for cancel and accept

--- micros-chess-lobby-service/src/it/java/io/github/marcodiri/lobbyservice/web/LobbyControllerIT.java
+++ micros-chess-lobby-service/src/it/java/io/github/marcodiri/lobbyservice/web/LobbyControllerIT.java
@@ -17,6 +17,8 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 
 import io.github.marcodiri.core.domain.event.DomainEvent;
+import io.github.marcodiri.lobbyservice.api.event.GameProposalAccepted;
+import io.github.marcodiri.lobbyservice.api.event.GameProposalCanceled;
 import io.github.marcodiri.lobbyservice.api.event.GameProposalCreated;
 import io.github.marcodiri.lobbyservice.domain.GameProposalFactory;
 import io.github.marcodiri.lobbyservice.repository.eventstore.EventStoreDBClientFactory;
@@ -60,6 +62,53 @@ public class LobbyControllerIT {
         UUID gameProposalId = response.getGameProposalId();
         List<DomainEvent> writtenEvents = repository.readEventsForAggregate(gameProposalId);
         assertThat(writtenEvents).containsOnly(new GameProposalCreated(gameProposalId, testPlayerId));
+    }
+
+    @Test
+    void cancelGameProposal()
+            throws StreamReadException, DatabindException, InterruptedException, ExecutionException, IOException {
+        RequestSpecification createRequest = with()
+                .contentType(ContentType.JSON)
+                .body("{ \"creatorId\" : \"" + testPlayerId + "\" }");
+
+        Response cratePost = createRequest.post("micros-chess/rest/lobby/create-game-proposal");
+
+        CreateGameProposalResponse response = cratePost.getBody().as(CreateGameProposalResponse.class);
+        UUID gameProposalId = response.getGameProposalId();
+        RequestSpecification cancelRequest = with()
+                .contentType(ContentType.JSON)
+                .body("{ \"gameProposalId\" : \"" + gameProposalId + "\", \"creatorId\" : \"" + testPlayerId
+                        + "\" }");
+
+        Response cancelPost = cancelRequest.post("micros-chess/rest/lobby/cancel-game-proposal");
+
+        cancelPost.then().assertThat().statusCode(equalTo(200));
+        List<DomainEvent> writtenEvents = repository.readEventsForAggregate(gameProposalId);
+        assertThat(writtenEvents.get(1)).isEqualTo(new GameProposalCanceled(gameProposalId));
+    }
+
+    @Test
+    void acceptGameProposal()
+            throws StreamReadException, DatabindException, InterruptedException, ExecutionException, IOException {
+        RequestSpecification createRequest = with()
+                .contentType(ContentType.JSON)
+                .body("{ \"creatorId\" : \"" + testPlayerId + "\" }");
+
+        Response cratePost = createRequest.post("micros-chess/rest/lobby/create-game-proposal");
+
+        CreateGameProposalResponse response = cratePost.getBody().as(CreateGameProposalResponse.class);
+        UUID gameProposalId = response.getGameProposalId();
+        UUID acceptorId = UUID.randomUUID();
+        RequestSpecification acceptRequest = with()
+                .contentType(ContentType.JSON)
+                .body("{ \"gameProposalId\" : \"" + gameProposalId + "\", \"acceptorId\" : \"" + acceptorId
+                        + "\" }");
+
+        Response acceptPost = acceptRequest.post("micros-chess/rest/lobby/accept-game-proposal");
+
+        acceptPost.then().assertThat().statusCode(equalTo(200));
+        List<DomainEvent> writtenEvents = repository.readEventsForAggregate(gameProposalId);
+        assertThat(writtenEvents.get(1)).isEqualTo(new GameProposalAccepted(gameProposalId, testPlayerId, acceptorId));
     }
 
 }
